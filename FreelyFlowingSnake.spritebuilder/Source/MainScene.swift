6c365462e725a7775dbb6702259bc203dae5b83e
Ikey Benzaken <ikey.benz@gmail.com>
2015-10-09 18:07:49 -0400

    Math implemented, slightly off. Need to edit ad fix

--- FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
+++ FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
@@ -2,27 +2,60 @@ import Foundation
 
 class MainScene: CCNode {
     var pieceArray: [CCNode] = []
-    var desiredRotation: Float!
+    var desiredRotation: Float = 44
     
     func didLoadFromCCB() {
         loadInitialPiece()
+        schedule("makeSnakeMove", interval: 2)
+        print(pieceArray[0].position)
     }
+    
     func loadInitialPiece() {
         let piece = CCBReader.load("Piece")
-        piece.position = ccp(100, 100)
+        piece.position = ccp(200, 200)
         addChild(piece)
-        pieceArray.append(piece)
+        pieceArray.insert(piece, atIndex: 0)
     }
-    func findNewPosition(currentPosition: CGPoint, angle: Float) -> CGPoint {
-        var newPosition: CGPoint!
-        if pieceArray[0].rotation == desiredRotation {
-            let x = 15 * sin(desiredRotation)
-            let y = 15 * cos(desiredRotation)
-            newPosition.x = currentPosition.x + CGFloat(x)
-            newPosition.y = currentPosition.y + CGFloat(y)
-        } else {
-            
+    func xComponent() -> Float {
+        if desiredRotation < 180 {
+            return Float(90)
+        } else if desiredRotation == 180 {
+            // Figure out what to do if desired Rotation is 180 or 0
         }
+        return Float(270)
+    }
+    func positiveOrNegativeX(number: Float) -> Float {
+        if desiredRotation > 0 && desiredRotation < 180 {
+            return number
+        } else {
+            return -number
+        }
+    }
+    func positiveOrNegativeY(number: Float) -> Float {
+        if desiredRotation < 90 && desiredRotation > 0 || desiredRotation > 270 && desiredRotation < 360 {
+            return -number
+        } else {
+            return number
+        }
+    }
+    func findNewPosition(currentPosition: CGPoint, movementAngle: Float) -> CGPoint {
+        var newPosition: CGPoint!
+        let y = (15 * cos(movementAngle))
+        let x = (15 * sin(movementAngle))
+        
+        newPosition = ccp(CGFloat(Float(currentPosition.x) + positiveOrNegativeX(x)), CGFloat(Float(currentPosition.y) + positiveOrNegativeY(y)))
+        print(newPosition)
         return newPosition
+    }
+    func makeSnakeMove() {
+        let lastPiece: CCNode = pieceArray[pieceArray.count - 1]
+        let newPiece = CCBReader.load("Piece")
+        let newPosition = findNewPosition(pieceArray[0].position, movementAngle: abs(xComponent() - desiredRotation))
+        newPiece.position = newPosition
+        newPiece.rotation = desiredRotation
+        removeChild(lastPiece)
+        pieceArray.removeAtIndex(pieceArray.indexOf(lastPiece)!)
+        addChild(newPiece)
+        pieceArray.insert(newPiece, atIndex: 0)
     }
 }
