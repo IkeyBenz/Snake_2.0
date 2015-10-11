e566cee77d42c2ca8834ef71307e7a0e0b5bd546
Ikey Benzaken <ikey.benz@gmail.com>
2015-10-11 04:01:56 -0400

    added joystick

--- FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
+++ FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
@@ -3,10 +3,13 @@ import Foundation
 class MainScene: CCNode {
     var pieceArray: [CCNode] = []
     var desiredRotation: Float = 44
+    weak var ball: CCSprite!
+    weak var base: CCSprite!
     
     func didLoadFromCCB() {
         loadInitialPiece()
-        schedule("makeSnakeMove", interval: 2)
+        userInteractionEnabled = true
+        schedule("makeSnakeMove", interval: 1)
         print(pieceArray[0].position)
     }
     
@@ -40,8 +43,9 @@ class MainScene: CCNode {
     }
     func findNewPosition(currentPosition: CGPoint, movementAngle: Float) -> CGPoint {
         var newPosition: CGPoint!
-        let y = (15 * cos(movementAngle))
-        let x = (15 * sin(movementAngle))
+        let y = 15 * cos(movementAngle)
+        let x = 15 * sin(movementAngle)
+        
         
         newPosition = ccp(CGFloat(Float(currentPosition.x) + positiveOrNegativeX(x)), CGFloat(Float(currentPosition.y) + positiveOrNegativeY(y)))
         print(newPosition)
@@ -52,10 +56,37 @@ class MainScene: CCNode {
         let newPiece = CCBReader.load("Piece")
         let newPosition = findNewPosition(pieceArray[0].position, movementAngle: abs(xComponent() - desiredRotation))
         newPiece.position = newPosition
-        newPiece.rotation = desiredRotation
+        newPiece.rotation = desiredRotation - 90
         removeChild(lastPiece)
         pieceArray.removeAtIndex(pieceArray.indexOf(lastPiece)!)
         addChild(newPiece)
         pieceArray.insert(newPiece, atIndex: 0)
     }
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        ball.position = touch.locationInNode(self)
+    }
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        let location = touch.locationInNode(self)
+        let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
+        let angle = atan2(v.dy, v.dx)
+        let degrees = angle * CGFloat(180 / M_PI)
+        print(degrees + 180)
+        desiredRotation = Float(degrees) + 180
+        
+        let length: CGFloat = base.boundingBox().height / 2
+        let xDist: CGFloat = sin(angle - 1.57079633) * length
+        let yDist: CGFloat = cos(angle - 1.57079633) * length
+        
+        if CGRectContainsPoint(base.boundingBox(), location) {
+            ball.position = location
+        } else {
+            ball.position = ccp(base.position.x - xDist, base.position.y + yDist)
+        }
+        
+    }
+    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        let move = CCActionMoveTo(duration: 0.2, position: base.position)
+        ball.runAction(move)
+    }
+    
 }
