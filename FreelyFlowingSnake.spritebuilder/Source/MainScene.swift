bc516a36480e1848525a4fb078384231d717ff93
Ikey Benzaken <ikey.benz@gmail.com>
2015-11-07 19:21:34 -0500

    good stuff

--- FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
+++ FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
@@ -28,14 +28,13 @@ class MainScene: CCNode {
         fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenHeight))))
         addChild(fruitPiece)
         slider.sliderValue = 0.5
-        schedule("makeSnakeMove", interval: 0.07)
+        schedule("makeSnakeMove", interval: 0.05)
 
     }
     override func update(delta: CCTime) {
         snakeAteFruit()
         detectGameover()
         updateSnakeRotation()
-        print(slider.sliderValue)
 
     }
     
@@ -53,6 +52,17 @@ class MainScene: CCNode {
         let y = 10 * sin(currentSnakeRotationInRadians)
         
         newPosition = ccp(CGFloat(Float(currentPosition.x) - x), CGFloat(Float(currentPosition.y) + y))
+        
+        if newPosition.x <= -15 {
+            newPosition.x = CCDirector.sharedDirector().viewSize().width
+        } else if newPosition.x >= CCDirector.sharedDirector().viewSize().width + 15 {
+            newPosition.x = 0
+        } else if newPosition.y <= -15 {
+            newPosition.y = CCDirector.sharedDirector().viewSize().height
+        } else if newPosition.y >= CCDirector.sharedDirector().viewSize().height + 15 {
+            newPosition.y = 0
+        }
+        
         return newPosition
     }
     func makeSnakeMove() {
@@ -79,8 +89,17 @@ class MainScene: CCNode {
     func snakeAteFruit() {
         if CGRectIntersectsRect(pieceArray[0].boundingBox(), fruitPiece.boundingBox()) {
             addSnakePiece()
-            fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenHeight))))
+            moveFruitPiece()
         }
+    }
+    func moveFruitPiece() {
+        fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenHeight))))
+        for piece in pieceArray {
+            if CGRectContainsRect(piece.boundingBox(), fruitPiece.boundingBox()) {
+                moveFruitPiece()
+            }
+        }
+
     }
     
     func detectGameover() {
@@ -98,45 +117,49 @@ class MainScene: CCNode {
         }
     }
     
+    func sliderControl() {
+        slider.sliderValue = 0.5
+    }
+    
     func updateSnakeRotation() {
-        let addedRotationInDegrees = (slider.sliderValue * 40) - 20
+        let addedRotationInDegrees = (slider.sliderValue * 30) - 15
         pieceRotation = pieceArray[0].rotation + addedRotationInDegrees
         desiredRotation = pieceRotation / Float(180 / M_PI)
         
     }
 
-//    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-//        base.position = touch.locationInWorld()
-//        ball.position = touch.locationInWorld()
-//        base.visible = true
-//        ball.visible = true
-//    }
-//    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-//        let location = touch.locationInNode(self)
-//        let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
-//        let angle = atan2(v.dy, v.dx)
-//        let degrees = angle * CGFloat(180 / M_PI)
-//        pieceRotation = -Float(degrees + 180)
-//        desiredRotation = Float(angle + 3.14159)
-//        
-//        let length: CGFloat = base.boundingBox().height / 2
-//        let xDist: CGFloat = sin(angle - 1.57079633) * length
-//        let yDist: CGFloat = cos(angle - 1.57079633) * length
-//        
-//        if CGRectContainsPoint(base.boundingBox(), location) {
-//            ball.position = location
-//        } else {
-//            ball.position = ccp(base.position.x - xDist, base.position.y + yDist)
-//        }
-//        
-//    }
-//    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-//        let move = CCActionMoveTo(duration: 0.2, position: base.position)
-//        ball.runAction(move)
-//        let delay = CCActionDelay(duration: 0.3)
-//        let hideBall = CCActionCallBlock(block: {self.ball.visible = false})
-//        let hideBase = CCActionCallBlock(block: {self.base.visible = false})
-//        runAction(CCActionSequence(array: [delay, hideBall, hideBase]))
-//    }
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        base.position = touch.locationInWorld()
+        ball.position = touch.locationInWorld()
+        base.visible = true
+        ball.visible = true
+    }
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        let location = touch.locationInNode(self)
+        let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
+        let angle = atan2(v.dy, v.dx)
+        let degrees = angle * CGFloat(180 / M_PI)
+        pieceRotation = -Float(degrees + 180)
+        desiredRotation = Float(angle + 3.14159)
+        
+        let length: CGFloat = base.boundingBox().height / 2
+        let xDist: CGFloat = sin(angle - 1.57079633) * length
+        let yDist: CGFloat = cos(angle - 1.57079633) * length
+        
+        if CGRectContainsPoint(base.boundingBox(), location) {
+            ball.position = location
+        } else {
+            ball.position = ccp(base.position.x - xDist, base.position.y + yDist)
+        }
+        
+    }
+    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        let move = CCActionMoveTo(duration: 0.2, position: base.position)
+        ball.runAction(move)
+        let delay = CCActionDelay(duration: 0.3)
+        let hideBall = CCActionCallBlock(block: {self.ball.visible = false})
+        let hideBase = CCActionCallBlock(block: {self.base.visible = false})
+        runAction(CCActionSequence(array: [delay, hideBall, hideBase]))
+    }
     
 }
