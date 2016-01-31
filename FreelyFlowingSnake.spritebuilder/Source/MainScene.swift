dd0cf908a5ad9bf0a35e18442dc942d7997c6cb5
Ikey Benzaken <ikey.benz@gmail.com>
2016-01-31 13:58:44 -0500

    A lot of good stuff
    - Implemented the follow feature
    - Made “snakeLand” bigger
    - Made “snakeLand” two separated parts with a door
    - Made the joysticks position fixed
    - Added a miniMap

--- FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
+++ FreelyFlowingSnake.spritebuilder/Source/MainScene.swift
@@ -1,15 +1,20 @@
 import Foundation
 
-class MainScene: CCNode {
+class MainScene: CCNode, CCPhysicsCollisionDelegate {
     
-    var pieceArray: [CCNode] = []
+    var pieceArray: [Piece] = []
+    var piecesInMiniMap: [Piece] = []
     var desiredRotation: Float = 3.14159
     var pieceRotation: Float = 130
+    var gameover = false
     
     let screenWidth = CCDirector.sharedDirector().viewSize().width
     let screenHeight = CCDirector.sharedDirector().viewSize().height
+    let screenwidthPercent = CCDirector.sharedDirector().viewSize().width / 100
+    let screenHeightPercent = CCDirector.sharedDirector().viewSize().height / 100
     
     let fruitPiece = CCBReader.load("Piece")
+    let fruitPieceOnMiniMap = CCBReader.load("Piece") as! Piece
     weak var background: CCNodeGradient!
     weak var screenFollowNode: CCNode!
     weak var gameoverLabel: CCLabelTTF!
@@ -17,35 +22,71 @@ class MainScene: CCNode {
     weak var base: CCSprite!
     weak var slider: CCSlider!
     weak var scoreLabel: CCLabelTTF!
+    weak var snakeLand: CCPhysicsNode!
+    weak var barrier1: CCNodeColor!
+    weak var barrier2: CCNodeColor!
+    weak var door: CCNodeColor!
+    weak var controls: CCNodeColor!
+    weak var miniMap: CCNodeColor!
     
     var score = 0 {
         didSet {
             scoreLabel.string = "Score: \(score)"
+            if score >= 5 {
+                door.visible = false
+            }
         }
     }
     
     func didLoadFromCCB() {
+        snakeLand.collisionDelegate = self
         loadInitialPiece()
         userInteractionEnabled = true
-        fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenHeight))))
-        addChild(fruitPiece)
+        snakeLand.addChild(fruitPiece)
+        miniMap.addChild(fruitPieceOnMiniMap)
+        fruitPieceOnMiniMap.position = ccp(CGFloat(arc4random_uniform(UInt32(miniMap.contentSizeInPoints.width))), CGFloat(arc4random_uniform(UInt32(miniMap.contentSizeInPoints.height))))
+        fruitPieceOnMiniMap.colorNode.color = CCColor(ccColor3b: ccColor3B(r: 255, g: 0, b: 0))
+        fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenWidth))))
         slider.sliderValue = 0.5
         schedule("makeSnakeMove", interval: 0.05)
-
+        setupDoubleTap()
+        sizeEverything()
     }
+    
     override func update(delta: CCTime) {
         checkIfSnakeAteFruit()
         detectGameover()
         updateSnakeRotation()
-
+    }
+    
+    func setupDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: "pauseGame")
+        doubleTap.numberOfTapsRequired = 2
+        CCDirector.sharedDirector().view.addGestureRecognizer(doubleTap)
+    }
+    
+    func pauseGame() {
+        if paused {
+            paused = false
+        } else {
+            paused = true
+        }
     }
     
     func loadInitialPiece() {
-        let piece = CCBReader.load("Piece")
-        piece.position = ccp(200, 200)
-        addChild(piece)
+        let piece = CCBReader.load("Piece") as! Piece
+        piece.position = ccp(320, 280)
+        snakeLand.addChild(piece)
         pieceArray.insert(piece, atIndex: 0)
     }
+    
+    func sizeEverything() {
+        controls.contentSizeInPoints.width = CCDirector.sharedDirector().viewSize().width
+        controls.contentSizeInPoints.height = CCDirector.sharedDirector().viewSize().width * CCDirector.sharedDirector().viewSize().width / CCDirector.sharedDirector().viewSize().height
+        miniMap.contentSizeInPoints.height = 0.75 * controls.contentSizeInPoints.height
+        miniMap.contentSizeInPoints.width = snakeLand.contentSizeInPoints.width * miniMap.contentSizeInPoints.height / snakeLand.contentSizeInPoints.height
+    }
+    
     func findNewPosition(currentPosition: CGPoint) -> CGPoint {
         var newPosition: CGPoint!
         let currentSnakeRotationInRadians = pieceArray[0].rotation / Float(180 / M_PI)
@@ -54,65 +95,81 @@ class MainScene: CCNode {
         let y = 10 * sin(currentSnakeRotationInRadians)
         
         newPosition = ccp(CGFloat(Float(currentPosition.x) - x), CGFloat(Float(currentPosition.y) + y))
-        
-        if newPosition.x <= -15 {
-            newPosition.x = CCDirector.sharedDirector().viewSize().width
-        } else if newPosition.x >= CCDirector.sharedDirector().viewSize().width + 15 {
-            newPosition.x = 0
-        } else if newPosition.y <= -15 {
-            newPosition.y = CCDirector.sharedDirector().viewSize().height
-        } else if newPosition.y >= CCDirector.sharedDirector().viewSize().height + 15 {
-            newPosition.y = 0
-        }
-        
+        followSnake(ccp(CGFloat(x), CGFloat(y)), newPosition: newPosition)
         return newPosition
     }
+    
     func makeSnakeMove() {
-        let lastPiece: CCNode = pieceArray[pieceArray.count - 1]
-        let newPiece = CCBReader.load("Piece")
+        let lastPiece: Piece = pieceArray[pieceArray.count - 1]
+        let newPiece = CCBReader.load("Piece") as! Piece
         let newPosition = findNewPosition(pieceArray[0].position)
+        
         newPiece.position = newPosition
         newPiece.rotation = pieceRotation
+        newPiece.colorNode.color = CCColor(ccColor3b: ccColor3B(r: 255, g: 0, b: 0))
         
-        removeChild(lastPiece)
+        snakeLand.removeChild(lastPiece)
         pieceArray.removeAtIndex(pieceArray.indexOf(lastPiece)!)
-
-
-        addChild(newPiece)
+        
+        snakeLand.addChild(newPiece)
+        updateMiniMap(newPosition)
         pieceArray.insert(newPiece, atIndex: 0)
+        if pieceArray.count > 1 {
+            pieceArray[1].colorNode.color = CCColor(ccColor3b: ccColor3B(r: 0, g: 255, b: 0))
+        }
     }
+    
+    func updateMiniMap(position: CGPoint) {
+        let piece = CCBReader.load("Piece") as! Piece
+        let x = miniMap.contentSizeInPoints.width * position.x / snakeLand.contentSizeInPoints.width
+        let y = miniMap.contentSizeInPoints.height * position.y / snakeLand.contentSizeInPoints.height
+        piece.position = ccp(x, y)
+        piece.scale = Float(miniMap.contentSizeInPoints.width / snakeLand.contentSizeInPoints.width)
+        miniMap.addChild(piece)
+        
+        if miniMap.children.count > score {
+            miniMap.removeChild(miniMap.children[1] as! CCNode)
+        }
+        
+    }
+    
     func addSnakePiece() {
         score++
-        let newPiece = CCBReader.load("Piece")
+        let newPiece = CCBReader.load("Piece") as! Piece
         newPiece.position = findNewPosition(pieceArray[0].position)
         newPiece.rotation = pieceRotation
+        newPiece.colorNode.color = CCColor(ccColor3b: ccColor3B(r: 0, g: 255, b: 0))
         pieceArray.insert(newPiece, atIndex: 0)
-        addChild(newPiece)
+        snakeLand.addChild(newPiece)
+        updateMiniMap(newPiece.position)
+        pieceArray[1].colorNode.color = CCColor(ccColor3b: ccColor3B(r: 0, g: 255, b: 0))
     }
+    
     func checkIfSnakeAteFruit() {
         if CGRectIntersectsRect(pieceArray[0].boundingBox(), fruitPiece.boundingBox()) {
             addSnakePiece()
             moveFruitPiece()
         }
     }
+    
     func moveFruitPiece() {
         fruitPiece.position = ccp(CGFloat(arc4random_uniform(UInt32(screenWidth))), CGFloat(arc4random_uniform(UInt32(screenHeight))))
-        for piece in pieceArray {
-            if CGRectContainsRect(piece.boundingBox(), fruitPiece.boundingBox()) {
-                moveFruitPiece()
-            }
-        }
+        fruitPieceOnMiniMap.position.x = miniMap.contentSizeInPoints.width * fruitPiece.position.x / snakeLand.contentSizeInPoints.width
+        fruitPieceOnMiniMap.position.y = miniMap.contentSizeInPoints.height * fruitPiece.position.y / snakeLand.contentSizeInPoints.height
+        fruitPieceOnMiniMap.scale = Float(miniMap.contentSizeInPoints.width / snakeLand.contentSizeInPoints.width)
     }
     
     func detectGameover() {
         for piece in pieceArray {
             if piece != pieceArray[0] && piece != pieceArray[1] && piece != pieceArray[2] && piece != pieceArray[3] {
-                if CGRectIntersectsRect(pieceArray[0].boundingBox(), piece.boundingBox()) {
+                if CGRectIntersectsRect(pieceArray[0].boundingBox(), piece.boundingBox()) && !gameover {
                     unschedule("makeSnakeMove")
+                    unschedule("addSnakePiece")
                     gameoverLabel.visible = true
                     let delay = CCActionDelay(duration: 1.5)
                     let restart = CCActionCallBlock(block: {CCDirector.sharedDirector().presentScene(CCBReader.loadAsScene("MainScene"))})
                     runAction(CCActionSequence(array: [delay, restart]))
+                    gameover = true
                 }
             }
         }
@@ -128,16 +185,15 @@ class MainScene: CCNode {
         desiredRotation = pieceRotation / Float(180 / M_PI)
         
     }
-    func adjustJoyStickRotation() {
+    
+    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, snakePiece nodeA: CCNode!, barrier nodeB: CCNode!) -> Bool {
+        print("Hit Barrier")
+        return true
+    }
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
         
     }
-
-    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-        base.position = touch.locationInWorld()
-        ball.position = touch.locationInWorld()
-        base.visible = true
-        ball.visible = true
-    }
+    
     override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
         let location = touch.locationInNode(self)
         let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
@@ -151,19 +207,57 @@ class MainScene: CCNode {
         let yDist: CGFloat = cos(angle - 1.57079633) * length
         
         if CGRectContainsPoint(base.boundingBox(), location) {
-            ball.position = location
+            ball.position = convertToNodeSpace(location)
+            
+            print(ball.position)
         } else {
             ball.position = ccp(base.position.x - xDist, base.position.y + yDist)
         }
         
     }
+    
     override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
         let move = CCActionMoveTo(duration: 0.2, position: base.position)
         ball.runAction(move)
-        let delay = CCActionDelay(duration: 0.3)
-        let hideBall = CCActionCallBlock(block: {self.ball.visible = false})
-        let hideBase = CCActionCallBlock(block: {self.base.visible = false})
-        runAction(CCActionSequence(array: [delay, hideBall, hideBase]))
     }
     
+    func followSnake(offSet: CGPoint, newPosition: CGPoint) {
+        let XlessThan25: Bool = newPosition.x + snakeLand.position.x < CCDirector.sharedDirector().viewSize().width / 100 * 49
+        let XgreaterThan75: Bool = newPosition.x + snakeLand.position.x > CCDirector.sharedDirector().viewSize().width / 100 * 51
+        let YlessThan35: Bool = newPosition.y + snakeLand.position.y < CCDirector.sharedDirector().viewSize().height / 100 * 79
+        let YgreaterThan65: Bool = newPosition.y + snakeLand.position.y > CCDirector.sharedDirector().viewSize().height / 100 * 81
+        let movingRight: Bool = abs(pieceArray[0].rotation) > 90 && abs(pieceArray[0].rotation) < 270
+        let movingLeft: Bool = abs(pieceArray[0].rotation) < 90 || abs(pieceArray[0].rotation) > 270
+        let movingDown: Bool = abs(pieceArray[0].rotation) < 180
+        let movingUp: Bool = abs(pieceArray[0].rotation) > 180
+        let notTooFarRight: Bool = (snakeLand.position.x + offSet.x <= 0)
+        let notTooFarLeft: Bool = (CCDirector.sharedDirector().viewSize().width - snakeLand.boundingBox().width < snakeLand.position.x + offSet.x)
+        let notTooFarUp: Bool = (snakeLand.position.y - offSet.y <= controls.boundingBox().height)
+        let notTooFarDown: Bool = (CCDirector.sharedDirector().viewSize().height - snakeLand.boundingBox().height < snakeLand.position.y - offSet.y)
+        
+        
+        if XlessThan25 && movingLeft && notTooFarRight {
+            snakeLand.position.x += CGFloat(offSet.x)
+        }
+        if XgreaterThan75 && movingRight && notTooFarLeft {
+            snakeLand.position.x += CGFloat(offSet.x)
+        }
+        if YlessThan35 && movingDown && notTooFarUp {
+            snakeLand.position.y -= CGFloat(offSet.y)
+        }
+        if YgreaterThan65 && movingUp && notTooFarDown {
+            snakeLand.position.y -= CGFloat(offSet.y)
+        }
+    }
+    
+    func snakeLandToMMPosition(position: CGPoint) -> CGPoint {
+        let x = miniMap.contentSizeInPoints.width * position.x / snakeLand.contentSizeInPoints.width
+        let y = miniMap.contentSizeInPoints.height * position.y / snakeLand.contentSizeInPoints.height
+        return ccp(x, y)
+    }
+    
+}
+
+class Piece: CCNode {
+    weak var colorNode: CCNodeColor!
 }
